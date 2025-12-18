e0a2b6081570e541b546fde122b6606276e96812
kurutis <cristina4n@yandex.ru>
2025-12-18 18:19:59 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: [], 
+    unoptimized: true, 
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
 }
 
 module.exports = nextConfig
